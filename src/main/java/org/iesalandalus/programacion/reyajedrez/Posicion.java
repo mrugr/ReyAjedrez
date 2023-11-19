f3b2cf2d3be7e1e4e9e4bfc17d6f6c86712854e0
mcrui <mcruizgarcia@gmail.com>
2023-11-19 10:08:15 +0100

    métodos getFila setFila y getColumna setColumna

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
@@ -1,6 +1,41 @@
 package org.iesalandalus.programacion.reyajedrez;
 
-public class Posicion {
-    private int fila;
-    private char columna;
-}
+    public class Posicion {
+        private int fila;
+        private char columna;
+
+    //Métodos GET y SET para atributos fila y columna
+
+        public int getFila(){ return fila;}
+
+        private void setFila(int fila) {
+            if (fila>=1 && fila<=8){
+                this.fila = fila;
+            } else{
+            //Excepción de tipo IllegalArgumentException
+            throw new IllegalArgumentException("ERROR:no es una fila válida");
+        }
+
+    }
+
+        public char getColumna(){return columna;}
+
+        private void setColumna( char columna){
+            if (columna >='a' && columna<='h'){
+                this.columna =columna;
+            }else{
+                //Excepción de tipo IllegalArgumentException
+                throw new IllegalArgumentException("ERROR:no es una columna válida");
+            }
+
+        }
+
+
+
+
+        }
+
+
+
+
+
